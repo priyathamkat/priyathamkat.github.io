57813992132798a8a2464fdd47daafc5e97af2c2
Priyatham Kattakinda <priyathamkat@gmail.com>
2024-10-22 19:34:31 -0400

    Setup basic layout

---
+++ src/app/components/ui/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar: React.FC = () => {
+    return (
+        <nav className="mx-auto max-w-5xl p-8">
+            <div className="text-2xl">Priyatham Kattakinda</div>
+        </nav>
+    );
+};
+
+export default Navbar;
